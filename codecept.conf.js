f734cca2e7f54043cb8fa20f6dfa07ccd400eeb2
Kirill Egorov <inu.grey@gmail.com>
2020-03-03 17:16:29 +0300

    move file into dirs

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,24 +1,24 @@
-const { setHeadlessWhen } = require('@codeceptjs/configure');
+const { setHeadlessWhen } = require("@codeceptjs/configure");
 
 // turn on headless mode when running with HEADLESS=true environment variable
 // HEADLESS=true npx codecept run
 setHeadlessWhen(process.env.HEADLESS);
 
 exports.config = {
-  tests: './*_test.js',
-  output: './output',
+  tests: "./tests/*_test.js",
+  output: "./output",
   helpers: {
     Puppeteer: {
-      url: 'http://blog.csssr.ru/qa-engineer/',
+      url: "http://blog.csssr.ru/qa-engineer/",
       show: false
     }
   },
   include: {
-    I: './steps_file.js'
+    I: "./steps_file.js"
   },
   bootstrap: null,
   mocha: {},
-  name: 'csssr-test-task',
+  name: "csssr-test-task",
   plugins: {
     retryFailedStep: {
       enabled: true
@@ -27,4 +27,4 @@ exports.config = {
       enabled: true
     }
   }
-}
+};
