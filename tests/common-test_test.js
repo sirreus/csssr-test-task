8e152eb2b88c9f1722c279519289a20314459d26
Kirill Egorov <inu.grey@gmail.com>
2020-03-03 18:17:44 +0300

    tune test, added few assert methods

--- tests/common-test_test.js
+++ tests/common-test_test.js
@@ -13,22 +13,53 @@ Scenario("Check link url", async I => {
   I.waitForElement('//div[@class="graphs-errors graph-active"]', 5);
   I.waitForVisible('//div[@class="info-errors"][@style="display: block;"]');
   I.seeElement('//a[text()="Софт для быстрого создания скриншотов"]');
+
+  // >> первый способ проверки (быстрый, общее время 3s) <<
   const actualLink = (
     await I.grabAttributeFrom(
       '//a[text()="Софт для быстрого создания скриншотов"]',
       "href"
     )
   ).split("://")[1];
-  if (expectedLink !== actualLink) {
-    I.say(
-      `Your actual link in href atribut ${actualLink} not equal to expected ${expectedLink}`
-    );
-    I.click('//a[text()="Софт для быстрого создания скриншотов"]');
-    I.switchToNextTab();
-    const currentLink = (await I.grabCurrentUrl()).split("://")[1];
-    assert.equal(currentLink, actualLink);
-    I.say(
-      `But redirect by actual link in href atribut ${actualLink} is corrects`
-    );
-  }
+  assert.equal(
+    actualLink,
+    expectedLink.split("://")[1],
+    `Your actual link in href atribut ${actualLink} NOT equal to expected ${
+      expectedLink.split("://")[1]
+    }`
+  );
+
+  // >> второй способ проверки (медленный, общее время 4s) <<
+  // const actualLink = (
+  //   await I.grabAttributeFrom(
+  //     '//a[text()="Софт для быстрого создания скриншотов"]',
+  //     "href"
+  //   )
+  // ).split("://")[1];
+  // if (actualLink.length > 0) {
+  //   I.click('//a[text()="Софт для быстрого создания скриншотов"]');
+  //   I.switchToNextTab();
+  //   I.seeInCurrentUrl(actualLink);
+  //   assert.equal(
+  //     actualLink,
+  //     expectedLink.split("://")[1],
+  //     `Your current link ${actualLink} NOT equal to expected ${
+  //       expectedLink.split("://")[1]
+  //     }`
+  //   );
+  // } else {
+  //   I.say("Attribut href at element < a > is empty");
+  // }
+
+  // >> третий способ проверки (еще медленней, общее время 5s) <<
+  // I.click('//a[text()="Софт для быстрого создания скриншотов"]');
+  // I.switchToNextTab();
+  // const currentLink = (await I.grabCurrentUrl()).split("://")[1];
+  // assert.equal(
+  //   currentLink,
+  //   expectedLink.split("://")[1],
+  //   `Your current link ${currentLink} NOT equal to expected ${
+  //     expectedLink.split("://")[1]
+  //   }`
+  // );
 });
